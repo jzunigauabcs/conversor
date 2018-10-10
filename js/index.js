475a988b1cf6e06d7fe3d862e9906c70b5e49859
Jesús Antonio Zúñiga Arce <jzuniga@uabcs.mx>
2018-10-10 07:26:12 -0600

    updaate

--- js/index.js
+++ js/index.js
@@ -1,121 +1,75 @@
-/*const numero1 = prompt("Introduce el número 1")
-const numero2 = prompt("Introduce el número 2")
-const numero3 = prompt("Introduce el número 3")
+const templateTemperature = function () {
+	return `<option value="gc">Grado Celsius</option>
+			<option value="gf">Grado Fahrenheit</option>`
+}
+const templateLongitude = function () {
+	return `<option value="cm">Centímetros</option>
+			<option value="mt">Metros</option>`
+}
 
-let mayor = 0
-let menor = 0
+const swapIndex = function (i) {
+	return i * -1 + 1
+}
 
-if(numero1 === numero2 && numero1 === numero3)
-	console.log('Los números son iguales')
-else {
-	if(numero1 >= numero2 && numero1 >= numero3) {
-		mayor = numero1
-	} else if(numero2 >= numero1 && numero2 >= numero3)  {
-		mayor = numero2
-	} else if(numero3 >= numero1 && numero3 >= numero2)  {
-		mayor = numero3
+const swap = function (i) {
+	let cbxMeasure = document.querySelectorAll('.cbxMeasure')
+	let otherIndex = swapIndex(i)
+	let otherValue = swapIndex(cbxMeasure[i].selectedIndex)
+	cbxMeasure[otherIndex].selectedIndex = otherValue
+}
+
+const changeMeasure = function (i) {
+	let cbxMeasure = document.querySelectorAll('.cbxMeasure')
+	if (cbxMeasure[0].value == cbxMeasure[1].value)
+		swap(i)
+}
+
+const executeCalc = function (value) {
+	let cbxMeasure = document.querySelectorAll('.cbxMeasure')
+	let functionName = `${cbxMeasure[0].value}To${cbxMeasure[1].value}`
+	let fn = window[functionName]
+	if(typeof fn === 'function') return fn()
+	else return ''
+}
+
+const calc = function (i) {
+	let result = document.querySelectorAll('.result')
+	if(isNaN(result[i].value)){
+		return
+	} else {
+		result[swapIndex(i)] = executeCalc(result[i].value)
 	}
+}
 
-	if(numero1 <= numero2 && numero1 <= numero3) {
-		menor = numero1
-	} else if(numero2 <= numero1 && numero2 <= numero3)  {
-		menor = numero2
-	} else if(numero3 <= numero1 && numero3 <= numero2)  {
-		menor = numero3
+const convertion = function (option) {
+	let cbxMeasure = document.querySelectorAll('.cbxMeasure')
+	let template
+	switch(parseInt(option)) {
+		case 1:
+			template = templateTemperature()
+			break
+		case 2:
+			template =  templateLongitude()
+			break
+		default: 
+			template =  ''
 	}
-	console.log('Número mayor: ' +  mayor)
-	console.log('Número menor: ' +  menor)
-}
-*/
-
-//const limite = prompt('Introduce el valor límite')
-/*for(let i = 0; i < limite; i++) {
-	console.log(i)
-}*/
-	/*let i = 0
-	while(i < limite) {
-		console.log(i)
-		i++
-	}*/
-/*	
-let found = false
-let attempt = 0
-const limit = 3
-let input
-const random = Math.floor(Math.random() * (10 - 1)) + 1
-console.log(random)
-while (attempt++ < limit && !found) {
-	input = prompt("Intrdouce el número a adivinar")
-	if(input == random)
-		found = true
-}
-if (found)
-	document.write('<h2 style="color: green">Número encontrado</h2')
-else 
-	document.write('<h2 style="color: red">Número no encontrado</h2')
-*/
-
-let cadena = 'Hola'
-/*for(let i = cadena.length - 1; i >= 0; i--) {
-	console.log(cadena.charAt(i))
-}*/
-
-//console.log(cadena.split('').reverse().join(''))
-
-// función declarativa
-/*
-
-let name = "María"
-console.log(saludo(name))
-
-function saludo(nombre) {
-	console.log(`Hola ${nombre}`)
+	for(let i = 0;i < cbxMeasure.length; i++){
+		cbxMeasure[i].innerHTML = template
+		cbxMeasure[i].value = cbxMeasure[i].options[i].value
+		cbxMeasure[i].onchange = changeMeasure.bind(this, i)
+	}
+	let functionName = `${cbxMeasure[0].value}To${cbxMeasure[1].value}`
+	let result = window[functionName]()
 }
 
 
-const saludo2 = function (nombre) {
-	return `Hola ${nombre}`
-}
-console.log(saludo2(name))
-
-const saludo3 = (nombre, edad) => {
-	return `Hola ${nombre} tu edad es ${edad} años`
-}
-console.log(saludo3('Juan', 30))
-
-
-function reverseName(name) {
-	return name.split('').reverse().join('')
-}
-/*
-'ana' => true
-'oso' => true
-'oro' => true
-'perro' => false
-'anita lava latina' => true
-
-let palindromo = "perro"
-function isPalindromo (word) {
-	let reverse = word.replace(/\s/g,'').split('').reverse().join('')
-	return word.replace(/\s/g,'') === reverse
+const gFTogc = function (temperatura) {
+	return 5 * (temperatura - 32) / 9
 }
 
-if(isPalindromo(palindromo)) {
-	console.log(`La palabra ${palindromo} es palíndromo`)
-} else {
-	console.log(`La palabra ${palindromo} no es palíndromo`)
-}
-
-*/
-
-const result = document.getElementById('result')
-
-const convertFtoC = function (temperatura) {
-	result.innerText = 5 * (temperatura - 32) / 9
-}
-
-const convertCtoF = function (temperatura) {
-	result.innerText = 9 * temperatura / 5 + 32
+const gcTogf = function (temperatura) {
+	return 9 * temperatura / 5 + 32
 }
 
 const calcTemp = function (option) {
